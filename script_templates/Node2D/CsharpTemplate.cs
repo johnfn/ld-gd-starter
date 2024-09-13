f203842350382ad2075999e8df3d0f7c3c39784d
johnfn <johnfn@gmail.com>
2024-09-13 17:11:11 -0400

    Starter LD code

---
+++ script_templates/Node2D/CsharpTemplate.cs
@@ -0,0 +1,14 @@
+// meta-default: true
+// meta-description: Blah
+
+using _BINDINGS_NAMESPACE_;
+using System;
+using static Utils;
+
+public partial class _CLASS_ : _BASE_ {
+  public override void _Ready() {
+  }
+
+  public override void _Process(double delta) {
+  }
+}
